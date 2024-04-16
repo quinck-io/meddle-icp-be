491b63e8ba29c92d58d27fedd84b05b0b6259ca9
NickGhignatti <nick.ghignatti@gmail.com>
2024-04-16 10:39:38 +0200

    feat(structure): :art: add trait for data and create the db structure
    add Storable trait on Data structure to make it savable in the stable memory and add a basic StableBTree to store data

--- src/meddle_backend/src/common/structures.rs
+++ src/meddle_backend/src/common/structures.rs
@@ -24,3 +24,18 @@ pub struct Data {
     value: f32,
     timestamp: u64,
 }
+
+impl Storable for Data {
+    fn to_bytes(&self) -> Cow<[u8]> {
+        Cow::Owned(serde_json::to_string(self).unwrap().as_bytes().to_vec())
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        serde_json::from_str(String::from_utf8(bytes.to_vec()).unwrap().as_str()).unwrap()
+    }
+
+    const BOUND: ic_stable_structures::storable::Bound = Bound::Bounded {
+        max_size: 1024,
+        is_fixed_size: false,
+    };
+}
---
+++ src/meddle_backend/src/database.rs
@@ -0,0 +1,13 @@
+use crate::common::structures::Data;
+
+thread_local! {
+
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static SENSOR_RECORDS: RefCell<StableBTreeMap<String, Data, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
+        )
+    );
+}
