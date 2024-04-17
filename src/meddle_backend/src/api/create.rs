8ed781985e2e3bdefa56a7139dfe2f1a3adbb849
EttoreFarinelli <ettore.farinelli@studio.unibo.it>
2024-04-17 12:53:46 +0200

    fix(create-api): :bug: fix return bug in post_data

--- src/meddle_backend/src/api/create.rs
+++ src/meddle_backend/src/api/create.rs
@@ -3,19 +3,13 @@ use crate::{common::{structures::{Data, JsonInput, OperationResult}, uuid::uuid_
 pub fn post_data(vec_json_data: Vec<JsonInput>) -> OperationResult {
 
     let mut ids: Vec<String> = Vec::new();
-    let mut code = 200;
     let mut local_timestamp = Option::None;
-    vec_json_data
-        .iter()
-        .for_each(|x| {
-            
+    for x in vec_json_data {  
             let unit_id = uuid_v4(local_timestamp);
             ids.push(unit_id.clone());
 
-            x.variables
-                .iter()
-                .for_each(|input| {
-                
+            for input in x.variables {
+
                     let res = insert_record( Data {
                         unit_id: unit_id.clone(),
                         sensor_id: input.sensorId.clone(),
@@ -24,20 +18,12 @@ pub fn post_data(vec_json_data: Vec<JsonInput>) -> OperationResult {
                     });
                 
                     if res.is_err() {
-                        code = 400;
-                        return;
+                        return OperationResult {unit_id: ids, code: 400, message: String::from("Error in writing elements in database")};
                     }
 
                     local_timestamp = Option::from(input.timestamp);
-                });
+                }
+        }
 
-            if code == 400 {
-                return;
-            }
-        });
-
-    if code == 400 {
-        return OperationResult {unit_id: ids, code, message: String::from("Error in writing elements in database")};
-    }
-    OperationResult {unit_id: ids, code, message: String::from("All Element Inserted Correctly")}
+    OperationResult {unit_id: ids, code: 200, message: String::from("All Element Inserted Correctly")}
 }
