f6b748d8c85ae27d857727ee0d1832365581574f
NickGhignatti <nick.ghignatti@gmail.com>
2024-04-19 16:17:19 +0200

    style(lib-api): :art: refactor code

--- src/meddle_backend/src/common/guards.rs
+++ src/meddle_backend/src/common/guards.rs
@@ -3,9 +3,8 @@ use ic_cdk::caller;
 
 pub fn caller_is_user() -> Result<(), String> {
     let caller = caller();
-    if caller != Principal::anonymous() {
-        Ok(())
-    } else {
-        Err("Caller is not the user of the canister.".to_string())
+    if caller == Principal::anonymous() {
+        return Err("Caller is not the user of the canister.".to_string());
     }
+    Ok(())
 }
